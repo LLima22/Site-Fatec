662cbd164b751c185aeb10744a5f4c58292cbde5
Lucas Limas <lucaslimagomes09@gmail.com>
2023-01-02 22:13:39 -0300

    first commit

---
+++ js/fonte.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var fonte = 14;
+    $('#aumentar').click(function(){
+        if(fonte<17){
+            fonte=fonte+1;
+            $('body').css({'font-size': fonte+'px'});
+        }
+    });
+    $('#diminuir').click(function(){
+        if(fonte>14){
+            fonte = fonte-1;
+            $('body').css({'font-size': fonte+'px'});
+        }
+    });
+});
